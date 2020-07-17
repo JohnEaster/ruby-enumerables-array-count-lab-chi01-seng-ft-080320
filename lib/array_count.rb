6133f6f2ac75b4785fb7aa8679423b7e9f188152
John Easter <john@owltestprep.com>
2020-07-17 13:48:17 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,4 +1,8 @@
-def count_strings(array)
+def count_strings(array = [1, "hello", [], 5.01, "world", :name, { a: 1 }])
+  array.count do
+    |element|
+    element.class == String?
+  
   # Return the total number of strings in the provided array using the count enumerable
 end
 
